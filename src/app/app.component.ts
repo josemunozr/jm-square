a6dd6c36bd8efc62ac6e2b9ccb1588f993b6eae5
José Muñoz Ruiz <jose.munoz@senegocia.com>
2018-12-17 18:50:11 -0300

    se crea component 'lugares' y 'detalle' y se enlaza con route correspondiente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jm-square';
-  lugares: any = [
-    { clicks: 0, plan: 'pagado', cercania: 1, distancia: 1, active: true, name: "Florería la Gardenia" },
-    { clicks: 0, plan: 'gratuito', cercania: 1, distancia: 1.8, active: false, name: "Donas la posadita" },
-    { clicks: 0, plan: 'gratuito', cercania: 2, distancia: 5, active: true, name: "Veterinaria Huellitas felices" },
-    { clicks: 0, plan: 'gratuito', cercania: 3, distancia: 10, active: true, name: "Sushi sushiroll" },
-    { clicks: 0, plan: 'pagado', cercania: 3, distancia: 35, active: true, name: "Hotel la gracia" },
-    { clicks: 0, plan: 'gratuito', cercania: 3, distancia: 120, active: true, name: "Zapateria el clavo" },
-  ];
 
-  lat: number = -33.4727092;
-  lng: number = -70.7699141;
 
   constructor() {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
 import { AgmCoreModule } from '@agm/core';
-import { ResaltarDirective } from './directives/resaltar.directive';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 import { ContarClicksDirective } from './directives/contar-clicks.directive';
+import { ResaltarDirective } from './directives/resaltar.directive';
+import { DetalleComponent } from "./detalle/detalle.component";
+import { LugaresComponent } from "./lugares/lugares.component";
+
+
+import { Routes, RouterModule } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: '', component: LugaresComponent },
+  { path: 'lugares', component: LugaresComponent }
+  { path: 'detalle', component: DetalleComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ResaltarDirective,
-    ContarClicksDirective
+    ContarClicksDirective,
+    DetalleComponent,
+    LugaresComponent
   ],
   imports: [
     BrowserModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDcl4gkDV4Rs2SrAxhZyfx-b3AepnUSC_Y'
-    })
+    }),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
