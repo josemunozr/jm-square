ecae44fe1637505842022d2cde73f169c0bd5a31
José Muñoz Ruiz <jose.munoz@senegocia.com>
2018-12-17 17:43:42 -0300

    HotBinding con directiva 'contar-clicks'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'jm-square';
   lugares: any = [
-    { plan: 'pagado', cercania: 1, distancia: 1, active: true, name: "Florería la Gardenia" },
-    { plan: 'gratuito', cercania: 1, distancia: 1.8, active: false, name: "Donas la posadita" },
-    { plan: 'gratuito', cercania: 2, distancia: 5, active: true, name: "Veterinaria Huellitas felices" },
-    { plan: 'gratuito', cercania: 3, distancia: 10, active: true, name: "Sushi sushiroll" },
-    { plan: 'pagado', cercania: 3, distancia: 35, active: true, name: "Hotel la gracia" },
-    { plan: 'gratuito', cercania: 3, distancia: 120, active: true, name: "Zapateria el clavo" },
+    { clicks: 0, plan: 'pagado', cercania: 1, distancia: 1, active: true, name: "Florería la Gardenia" },
+    { clicks: 0, plan: 'gratuito', cercania: 1, distancia: 1.8, active: false, name: "Donas la posadita" },
+    { clicks: 0, plan: 'gratuito', cercania: 2, distancia: 5, active: true, name: "Veterinaria Huellitas felices" },
+    { clicks: 0, plan: 'gratuito', cercania: 3, distancia: 10, active: true, name: "Sushi sushiroll" },
+    { clicks: 0, plan: 'pagado', cercania: 3, distancia: 35, active: true, name: "Hotel la gracia" },
+    { clicks: 0, plan: 'gratuito', cercania: 3, distancia: 120, active: true, name: "Zapateria el clavo" },
   ];
 
   lat: number = -33.4727092;
--- src/app/directives/contar-clicks.directive.ts
+++ src/app/directives/contar-clicks.directive.ts
@@ -1,12 +1,16 @@
-import { Directive, HostListener } from "@angular/core";
+import { Directive, HostListener, HostBinding, Input } from "@angular/core";
 
 @Directive({
-	selector: 'a[contar-clicks]'
+	selector: 'li[contar-clicks]'
 })
 
+
 export class ContarClicksDirective {
+	@Input('contar-clicks') lugar: any;
 	clicksN = 0;
+	@HostBinding('style.opacity') opacity: number = .1
 	@HostListener('click', ['$event.target']) onclick(btn) {
 		console.log('a', btn, "Número de clicks", this.clicksN++);
+		this.opacity += .1
 	}
 }
