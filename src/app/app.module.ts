41d2569f23f7a95c03dd6e26e268c95eb757282c
José Muñoz Ruiz <jose.munoz@senegocia.com>
2018-12-18 18:16:44 -0300

    se implementa routerLink como remplazo de href para salto entre routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { Routes, RouterModule } from "@angular/router";
 
 const appRoutes: Routes = [
   { path: '', component: LugaresComponent },
-  { path: 'lugares', component: LugaresComponent }
+  { path: 'lugares', component: LugaresComponent },
   { path: 'detalle', component: DetalleComponent }
 ];
 
