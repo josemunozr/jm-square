ba923498ce5d313c00a74524703ee94146695e5c
José Muñoz Ruiz <jose.munoz@senegocia.com>
2018-12-20 12:35:29 -0300

    se agrega firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { ContactoComponent } from "./contacto/contacto.component";
 
 import { LugaresService } from "./services/lugares.service";
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { Routes, RouterModule } from "@angular/router";
 
@@ -19,6 +22,18 @@ const appRoutes: Routes = [
   { path: 'detalle/:id', component: DetalleComponent },
   { path: 'contacto', component: ContactoComponent }
 ];
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCCTyzGFTkUKEl4MoiX0PmzCfO3tgNm0oU",
+    authDomain: "jm-square-1544016241774.firebaseapp.com",
+    databaseURL: "https://jm-square-1544016241774.firebaseio.com",
+    projectId: "jm-square-1544016241774",
+    storageBucket: "jm-square-1544016241774.appspot.com",
+    messagingSenderId: "123125301657"
+  }
+};
 
 @NgModule({
   declarations: [
@@ -34,7 +49,10 @@ const appRoutes: Routes = [
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDcl4gkDV4Rs2SrAxhZyfx-b3AepnUSC_Y'
     }),
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(environment.firebase, 'jm-square'),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [
     LugaresService
