57b4a430eac7b24cd543e4705faaf6fdb5091368
José Muñoz R <jozeepp@gmail.com>
2019-01-13 19:05:10 -0300

    se implementa animacion de prueba con angular. trigger, state, transition y animate usados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { AgmCoreModule } from '@agm/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms'
 import { HttpModule } from "@angular/http";
 import { AppComponent } from './app.component';
@@ -42,6 +43,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
+    BrowserAnimationsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDcl4gkDV4Rs2SrAxhZyfx-b3AepnUSC_Y'
     }),
--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { LugaresService } from '../services/lugares.service';
+import { trigger, state, style, transition, animation, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-lugares',
   templateUrl: './lugares.component.html',
-  styleUrls: ['./lugares.component.css']
+  styleUrls: ['./lugares.component.css'],
+  animations: [
+    trigger('contenedorAnimable', [
+      state('inicial', style({
+        opacity: 0,
+        backgroundColor: 'green',
+        transform: 'rotate3d(0,0,0,0deg)'
+      })),
+      state('final', style({
+        opacity: 1,
+        backgroundColor: 'yellow',
+        transform: 'rotate3d(5,10,20,30deg)'
+      })),
+      transition('inicial => final', animate(1000)),
+      transition('final => inicial', animate(500)),
+    ])
+  ]
 })
 
 export class LugaresComponent {
   title = 'jm-square';
+  state = 'inicial';
 
   lat: number = -33.4727092;
   lng: number = -70.7699141;
@@ -22,5 +40,9 @@ export class LugaresComponent {
       }, error => {
         alert(`Tenemos dificultades, disculpe las molestias. Error : ${error.statusText}`);
       });
+  }
+
+  animar() {
+    this.state = this.state == 'final' ? 'inicial' : 'final'
   }
 }
