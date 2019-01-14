1df0b4f593dd997896e4a3b5977855d7d3be4680
José Muñoz Ruiz <jose.munoz@senegocia.com>
2019-01-14 12:34:15 -0300

    se aplica animación a lugares destacados

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { LugaresService } from '../services/lugares.service';
-import { trigger, state, style, transition, animation, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-lugares',
@@ -9,17 +9,13 @@ import { trigger, state, style, transition, animation, animate } from '@angular/
   animations: [
     trigger('contenedorAnimable', [
       state('inicial', style({
-        opacity: 0,
-        backgroundColor: 'green',
-        transform: 'rotate3d(0,0,0,0deg)'
+        opacity: 0
       })),
       state('final', style({
-        opacity: 1,
-        backgroundColor: 'yellow',
-        transform: 'rotate3d(5,10,20,30deg)'
+        opacity: 1
       })),
-      transition('inicial => final', animate(1000)),
-      transition('final => inicial', animate(500)),
+      transition('inicial=>final', animate(2000)),
+      transition('final=>inicial', animate(1000)),
     ])
   ]
 })
@@ -37,12 +33,9 @@ export class LugaresComponent {
       .subscribe(response => {
         let data = response;
         this.lugares = Object.keys(data).map(key => data[key])
+        this.state = 'final'
       }, error => {
         alert(`Tenemos dificultades, disculpe las molestias. Error : ${error.statusText}`);
       });
-  }
-
-  animar() {
-    this.state = this.state == 'final' ? 'inicial' : 'final'
   }
 }
