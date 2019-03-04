04db3561913fbe1266bce29d1fb05ebce92bf6b1
José Muñoz Ruiz <jose.munoz@senegocia.com>
2019-03-04 17:42:07 -0300

    implementación de cierre de sesión

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AutorizacionService } from './services/autorizacion.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,19 @@ import { AutorizacionService } from './services/autorizacion.service';
 export class AppComponent {
   loggedIn: boolean = false;
 
-  constructor(private autorizacionServie: AutorizacionService) {
+  constructor(private autorizacionServie: AutorizacionService, private router: Router) {
     this.autorizacionServie.isLogged()
       .subscribe(result => {
         this.loggedIn = (result && result.uid) ? true : false;
       }, error => {
         this.loggedIn = false
       })
+  }
+
+  logout() {
+    this.autorizacionServie.logout();
+    alert("Sesión cerrada correctamente");
+    this.router.navigate(['lugares']);
   }
 
 }
--- src/app/services/autorizacion.service.ts
+++ src/app/services/autorizacion.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AutorizacionService {
 
-  constructor(private angularFireAuth: AngularFireAuth) {
+  constructor(private angularFireAuth: AngularFireAuth, private router: Router) {
     console.log(this.isLogged());
   }
 
@@ -13,6 +14,7 @@ export class AutorizacionService {
     this.angularFireAuth.auth.signInWithEmailAndPassword(email, password)
       .then(response => {
         alert('Login OK')
+        this.router.navigate(['lugares']);
       })
       .catch(error => {
         alert(`error al logearse: ${error}`);
@@ -24,6 +26,7 @@ export class AutorizacionService {
     this.angularFireAuth.auth.createUserWithEmailAndPassword(email, password)
       .then(response => {
         alert('Regristo OK')
+        this.router.navigate(['lugares']);
       })
       .catch(error => {
         alert(`error al registrarse: ${error}`);
@@ -32,5 +35,9 @@ export class AutorizacionService {
 
   public isLogged() {
     return this.angularFireAuth.authState;
+  }
+
+  public logout() {
+    return this.angularFireAuth.auth.signOut();
   }
 }
